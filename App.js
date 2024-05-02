c02c38fd738a251186fa997210c2667cc9945208
unknown <divyaojha2123@gmail.com>
2024-05-02 16:03:01 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,6 @@
+
+       const hs = React.createElement('h1',{},'hello wordl by using react')
+       const root =ReactDOM.createRoot(document.getElementById('root'))
+       root.render(hs)
+       console.log(root)
+    
